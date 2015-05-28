38d8cfc32a927c9e7ae97d3590143e3b205e7aa4
Mark Hoffman <mh4913@att.com>
2015-05-28 07:46:53 -0400

    Updates

--- js/app.js
+++ js/app.js
@@ -1,13 +1,35 @@
+var Cat = function() {
+	var self = this;
+	self.clickCount = ko.observable(0);
+	self.name = ko.observable('Tabby');
+	self.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
+	self.imgAttribution = ko.observable('https://www.flickr.com/photos/big');
+	
+	
+	self.findCatType = ko.computed(function() {
+		
+		if(self.clickCount() < 10) {
+			return "Newborn";
+		} else if (self.clickCount() >= 10 && self.clickCount() < 20) {
+			return "Infant";
+		} else {
+			return "Teen";
+		}	
+	});
+	
+    self.catNames = ko.observableArray([
+        { name: 'Kookla' },
+        { name: 'Fran' },
+        { name: 'Ollie' },
+        { name: 'Oscar'}
+    ]);
+}
+
 var ViewModel = function() {
-	this.clickCount = ko.observable(0);
-	this.name = ko.observable('Tabby');
-	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-	this.imgAttribution = ko.observable('https://www.flickr.com/photos/big');
-	
-	this.incrementCounter = function() {
-		this.clickCount(this.clickCount() + 1);
+	self.currentCat = ko.observable(new Cat() );
+	self.incrementCounter = function() {
+		self.currentCat().clickCount(self.currentCat().clickCount() + 1);
 	};
-	
 }
 
 ko.applyBindings(new ViewModel());
